ff8400cc36c48e95fab192e0b5066e5d96ba00fe
hl <517130611@qq.com>
2022-05-24 14:41:40 +0800

    first commit

---
+++ src/main/java/com/slxx/controller/ordercontroller.java
@@ -0,0 +1,21 @@
+package com.slxx.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+
+@RestController
+public class ordercontroller {
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/order/docker")
+    public String helloDocker() {
+        return "hello docker " + "\t" + port + "\t" + UUID.randomUUID().toString();
+    }
+
+}
---
+++ src/test/java/com/slxx/DockerBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
